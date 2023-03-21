4fe99663279735e806aeb119643b78273828e4b6
hoang6918 <80245864+hoang6918@users.noreply.github.com>
2023-03-21 10:05:37 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,6 +9,21 @@ function DHT20 () {
     NPNLCD.ShowNumber(dht11_dht22.readData(dataType.temperature), 0, 0)
     NPNLCD.ShowNumber(dht11_dht22.readData(dataType.humidity), 0, 1)
 }
+function TEMP_AND_FAN () {
+    if (input.temperature() >= 35) {
+        basic.showNumber(input.temperature())
+        basic.showString("Hot")
+        basic.clearScreen()
+    } else if (input.temperature() <= 12) {
+        basic.showNumber(input.temperature())
+        basic.showString("Cold")
+        basic.clearScreen()
+    } else {
+        basic.showNumber(input.temperature())
+        basic.clearScreen()
+    }
+    basic.pause(2000)
+}
 function MOISTURE_AND_PUMP () {
     if (NPNBitKit.AnalogSoilMosture(AnalogPin.P0) < 30) {
         NPNBitKit.Relay(DigitalPin.P2, true)
@@ -29,9 +44,11 @@ WiFiBit.connectToWiFiNetwork("SSID", "key")
 ESP8266_IoT.adafruit_setting("tringuyennek", "123456789")
 NPNLCD.LcdInit(39)
 NPNLCD.on()
+let temp = input.temperature()
 basic.forever(function () {
     NPNLCD.clear()
     DHT20()
     MOISTURE_AND_PUMP()
     LIGHT_AND_LED()
+    TEMP_AND_FAN()
 })
